5933a8e1c259915fedf1cf7bd4ccc1079aa68204
iStaha <tahalatief@email.com>
2019-10-23 09:23:38 +0800

    Updates

---
+++ precache-manifest.bc43eee7de43531101bb106d5e392276.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe91c5f32ddecf3df3671b9981653f98",
+    "url": "/react-list/index.html"
+  },
+  {
+    "revision": "da4342be12bd1dbf35b1",
+    "url": "/react-list/static/css/main.2d2272db.chunk.css"
+  },
+  {
+    "revision": "7c2ab30cdb4f747bd9cd",
+    "url": "/react-list/static/js/2.282687f7.chunk.js"
+  },
+  {
+    "revision": "da4342be12bd1dbf35b1",
+    "url": "/react-list/static/js/main.2f0e9025.chunk.js"
+  },
+  {
+    "revision": "75e8454445ec9aa9737f",
+    "url": "/react-list/static/js/runtime~main.4bcfde94.js"
+  }
+]);
